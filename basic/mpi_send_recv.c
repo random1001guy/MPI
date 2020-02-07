8a7f1a61b294997c08ea45174dfe930fdae0b503
random1001guy <random1001guy@gmail.com>
2020-02-08 02:30:47 +0530

    updated mpi_send_recv.c

--- basic/mpi_send_recv.c
+++ basic/mpi_send_recv.c
@@ -12,11 +12,15 @@ int main(int argc, char *argv[]){
 	
 	if(rank==0){
 		number = 23;
+		printf("\nHey, this is process with rank %d. I'm sending the number %d to process with rank 1.\n", rank, number );
 		MPI_Send(&number,1,MPI_INT,1,0,MPI_COMM_WORLD);
 	}
 	else if (rank == 1){
 		MPI_Recv(&number,1,MPI_INT,0,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-		printf("I received the number %d from the process with rank 0. My rank is %d.\n",number ,rank);
+		printf("\nI received the number %d from the process with rank 0. My rank is %d.\n",number ,rank);
+	}
+	else{
+		printf("\nI'm the process with rank %d\n",rank);
 	}
 
 	MPI_Finalize();
